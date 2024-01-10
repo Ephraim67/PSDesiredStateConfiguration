ad5826181c2bb9222dec4a491ecc0ebff45ce13f
Ephraim Christian Norbert <90705943+Ephraim67@users.noreply.github.com>
2024-01-10 15:28:06 +0100

    Add files via upload

---
+++ MyDscConfiguration.ps1
@@ -0,0 +1,29 @@
+﻿Configuration MyDscConfiguration
+{
+   param
+   (
+      [string[]]$ComputerName='localhost'
+   )
+
+   Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+   Node $ComputerName
+   {
+      WindowsFeature MyFeatureInstance
+      
+      {
+         Ensure = 'Present'
+         Name = 'RSAT'
+      }
+
+      WindowsFeature My2ndFeatureInstance
+      {
+         Ensure = 'Present'
+         Name = 'Bitlocker'
+      }
+
+   }
+
+}
+
+MyDscConfiguration
